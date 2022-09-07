b3f0b1b9e88448f6d7e270d5a95183ba8f59e1df
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-09-07 16:35:17 +0000

    🎉 initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const KEY = 'coospace-pw';
+const button = document.querySelector('input[type="submit"]');
+const passHolder = document.querySelector('#password');
+button.addEventListener('click', () => {
+	const pw = passHolder.value;
+	localStorage.setItem(KEY, pw);
+});
+
+const pw = localStorage.getItem(KEY);
+if (pw) {
+	passHolder.value = pw;
+	console.log(window.history);
+	button.click();
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const path = require("path")
+const fs = require("fs")
+const WebpackUserscript = require('webpack-userscript')
+const dev = process.env.NODE_ENV == 'dev'
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  entry: fs.readdirSync('src').map(item => {
+    const name = item.replace('.js', '')
+    return { key: name, value: `./src/${item}` }
+  }).filter(e=>e.key.includes('-public')||e.key=="index")
+  .reduce((obj, item) => (obj[item.key] = item.value, obj), {}),
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].user.js',
+    chunkFilename: '[name]',
+    publicPath: '',
+  },
+  optimization: {
+    minimize: false,
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new WebpackUserscript({
+      headers: {
+        name: '[name]',
+        version: `[version].[buildTime]`,
+        namespace: "http://github.com/KNDQJI/coospace-quick-login",
+        match: "https://www.coosp.etr.u-szeged.hu/",
+        require: [],
+        resource: [],
+        grant: []
+      },
+      downloadBaseUrl: dev ? "http://localhost:8080" : "https://github.com/KNDQJI/coospace-quick-login/releases/download/latest/",
+      updateBaseUrl: dev ? "http://localhost:8080" : "https://github.com/KNDQJI/coospace-quick-login/releases/download/latest/",
+    })
+  ]
+}
