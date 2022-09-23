11d8436dfa0eb16dcc7864764678b587eeee2db7
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-09-23 09:59:58 +0200

    chore: only match login screen

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const KEY = 'coospace-pw';
-const button = document.querySelector('input[type="submit"]');
-const passHolder = document.querySelector('#password');
-button.addEventListener('click', () => {
-	const pw = passHolder.value;
-	localStorage.setItem(KEY, pw);
-});
+if (window.location.href.split('?')[0] == 'https://www.coosp.etr.u-szeged.hu/') {
 
-const pw = localStorage.getItem(KEY);
-if (pw) {
-	passHolder.value = pw;
-	button.click();
+	const KEY = 'coospace-pw';
+	const button = document.querySelector('input[type="submit"]');
+	const passHolder = document.querySelector('#password');
+	button.addEventListener('click', () => {
+		const pw = passHolder.value;
+		localStorage.setItem(KEY, pw);
+	});
+
+	const pw = localStorage.getItem(KEY);
+	if (pw) {
+		passHolder.value = pw;
+		button.click();
+	}
 }
