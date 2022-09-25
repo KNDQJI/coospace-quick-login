a7a044d4f37a97686529af7a622487e12961212a
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-09-25 22:25:37 +0200

    chore: hide fixed spaces

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 if (window.location.href.split('?')[0] == 'https://www.coosp.etr.u-szeged.hu/') {
+  const KEY = 'coospace-pw';
+  const button = document.querySelector('input[type="submit"]');
+  const passHolder = document.querySelector('#password');
+  button.addEventListener('click', () => {
+    const pw = passHolder.value;
+    localStorage.setItem(KEY, pw);
+  });
 
-	const KEY = 'coospace-pw';
-	const button = document.querySelector('input[type="submit"]');
-	const passHolder = document.querySelector('#password');
-	button.addEventListener('click', () => {
-		const pw = passHolder.value;
-		localStorage.setItem(KEY, pw);
-	});
-
-	const pw = localStorage.getItem(KEY);
-	if (pw) {
-		passHolder.value = pw;
-		button.click();
-	}
+  const pw = localStorage.getItem(KEY);
+  if (pw) {
+    passHolder.value = pw;
+    button.click();
+  }
+}
+if (window.location.href.split('?')[0] == 'https://www.coosp.etr.u-szeged.hu/Home') {
+  [...document.querySelectorAll('.scenes>ul.compact')][0].style.display = 'none';
 }
